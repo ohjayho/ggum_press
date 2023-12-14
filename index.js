d783095118c65fbbe61a031c4937559cea35c95b
Jay Ho Oh <ohjayhoi@gmail.com>
2023-12-14 20:48:02 +0900

    docs: add comment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const btn = document.querySelector(".btn_contact");
-btn.addEventListener("click", function () {
+const btn_contact = document.querySelector(".btn_contact");
+
+//link the contact button to contact page on Footer section
+btn_contact.addEventListener("click", function () {
   window.location.href = "./contact.html";
 });
